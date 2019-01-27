0b3b18e1c280dc7dbde787bc37498fc7f15ccde2
vuongba <bavuongvng@gmail.com>
2019-01-27 08:56:17 +0700

    Initial Commit

---
+++ GoCheckin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoCheckin
+//
+//  Created by VuongHN on 1/27/19.
+//  Copyright © 2019 VuongHN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
